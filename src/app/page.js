08048938b0d503fb1680c2858432b29557f2e8ff
Ed Brown <edb56@live.co.uk>
2025-02-26 20:41:09 +0000

    app import fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import App from "./components/app.js";
+import App from "./components/App.js";
 
 export default function Home() {
   return <App />;
